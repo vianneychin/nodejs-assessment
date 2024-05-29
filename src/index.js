de9954179e02c8c7968e91c94e50bb9df49db35b
Anderson Menezes <andersonmenezes.dev@gmail.com>
2024-05-29 13:47:53 -0300

    feat(api): finish sample project code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-const express = require('express');
+global.PORT = 3000;
 
+const express = require('express');
 const app = express();
 
-app.use(express.json())
+const userController = require('./usercontroller');
 
+app.get('/users', userController.getAllUsers);
+app.post('/users', userController.createUser);
+app.get('/users/:id', userController.getUserById);
+app.put('/users/:id', userController.updateUser);
+app.delete('/users/:id', userController.deleteUser);
+
+app.use(express.json())
 
 app.get('/', (req,res) => {
 	res.set('Content-Type', 'text/html');
 	res.status(200).send('<h1>Hello World</h1>')
 })
 
-app.listen(3000, (error,) => {
+app.listen(global.PORT, (error,) => {
 	if (!error) {
-		console.info(`App successfully started and is listening on port ` + 3000)
+		console.info(`App successfully started and is listening on port ` + global.PORT)
 		return;
 	}
 	
-	console.info(`Error occured, server can't start`, error)
+	console.info(`Error occurred, server can't start`, error)
 })
